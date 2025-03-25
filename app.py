6186bbeee656e86ef455d23c23c2aca17080113a
avsgsta <155411458+avsgsta@users.noreply.github.com>
2025-03-26 03:07:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ import os
 import gdown
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
